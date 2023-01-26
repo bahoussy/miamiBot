88212cd128493c63b3cf602313c18b303c386a4b
Houssem kouki <housskouki3@gmail.com>
2023-01-26 14:04:17 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,58 +2,61 @@ import os
 import requests
 import discord
 from dotenv import load_dotenv 
-from keep_alive import keep_alive
 from bs4 import BeautifulSoup
 from datetime import datetime
-from threading import Timer
-from replit import db
+import pymongo
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
-x=datetime.today()
-y=x.replace(day=x.day+1, hour=1, minute=0, second=0, microsecond=0)
-delta_t=y-x
 
-secs=delta_t.seconds+1
 
-def daily_log():
-  logs =''
-  rsp = requests.get("https://www.local10.com")
-  soup = BeautifulSoup(rsp.text, 'html.parser')
-  blog_titles = soup.findAll('figure')
-  for title in range(len(blog_titles[6:15])):
-    logs += '- '+blog_titles[title].findChildren("div")[0].findChildren("div")[0]['aria-label'] +'\n'
-  db["{}".format(x)] = logs
-  print(db.keys())
+async def daily_log(message):
   
-client = discord.Client()
+  client = pymongo.MongoClient("mongodb+srv://houssem:bahoussykouki38@cluster0.igw9k.mongodb.net/?retryWrites=true&w=majority")
+  client.test
+  print(client)
+  db = client.discord_logs
+  log_collection = db.log
+
+  entry = {}
+  entry['timestamp'] = datetime.now()
+  entry['msg'] = message.content
+  entry['user'] = message.author.name
+  entry['user_id'] = message.author.id
+  log_collection.insert_one(entry)
+  print("logged :",entry)
+
+client = discord.Client(intents=discord.Intents.all())
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
 @client.event
 async def on_message(message):
+  try:
+    if message.author == client.user:
+      return
+    print("Message : ",message.content)
+    await daily_log(message)
     if client.user.mentioned_in(message):
         await message.channel.send(file=discord.File('Miami.mp4'))
-    if message.author == client.user:
-        return
-    news =''
+
+
     if message.content == '!miami':
-      
+      print("Message received")
+      news =''
       rsp = requests.get("https://www.local10.com")
       soup = BeautifulSoup(rsp.text, 'html.parser')
       blog_titles = soup.findAll('figure')
       for title in range(len(blog_titles[6:15])):
-        
         news += '- '+blog_titles[title].findChildren("div")[0].findChildren("div")[0]['aria-label'] +'\n'
       response = "<@{}> **Here is what the fuck going on in miami : **".format(message.author.id)
-      try: 
-        await message.channel.send(response)
-        await message.channel.send(news)
-      except Exception as e:
+      
+      await message.channel.send(response)
+      await message.channel.send(news)
+  except Exception as e:
         print(e)
         await message.channel.send("<@{}> Can't fetch Miami news for you. Could be either an internal error or my code is shit. Either way go goole it yourself.".format(message.author.id))
-keep_alive()
-t = Timer(secs, daily_log)
-t.start()
+  
+
 client.run(TOKEN)
